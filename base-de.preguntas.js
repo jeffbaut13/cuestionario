98b334e2d20ca9840d17135125e2b8ce62c267e4
jeffbaut13 <jeferssonabv@gmail.com>
2022-09-23 17:53:43 -0500

    inicio

---
+++ base-de.preguntas.js
@@ -0,0 +1,77 @@
+
+let baseDePreguntas = [
+
+  {
+    pregunta: "¿Que residuos tirarías en la bolsa blanca de reciclaje?",
+    ayuda:"territorio",
+    respuesta: "<i class='fa-solid fa-circle'></i><span style='padding-left:4%;'><strong>Botellas de agua,</strong> cartón, vidrio, metal</span>",
+    distractores: [
+      "<i class='fa-solid fa-circle'></i><span style='padding-left:4%;'><strong>Residuos</strong> de comida, cascaras de huevo</span>", 
+      "<i class='fa-solid fa-circle'></i><span style='padding-left:4%;'><strong>Servilletas</strong> usadas y papel higiénico</span>",
+      "<i style='display:none;' class='fa-solid fa-circle'></i><span style='display:none;padding-left:4%;'></span>"
+    ]
+  },
+  {
+    pregunta: "¿En cuál bolsa tirarías las botellas PET aceite, botellas PET color, empaques de lechuga, arándanos y fresas?",
+    respuesta: "<i class='fa-solid fa-circle'></i><span style='padding-left:4%;'><strong>Blanca</strong></span>",
+    distractores: [
+      "<i class='fa-solid fa-circle'></i><span style='padding-left:4%;'><strong>Verde</strong></span>", 
+      "<i class='fa-solid fa-circle'></i><span style='padding-left:4%;'><strong>Negra</strong></span>",
+      "<i style='display:none;' class='fa-solid fa-circle'></i><span style='display:none;padding-left:4%;'></span>"
+    ]
+  },
+  {
+    pregunta: "¿Por que crees que una botella de colores tienes retos en el reciclaje?",
+    respuesta: "<i class='fa-solid fa-circle'></i><span style='padding-left:4%;'>por su <strong>color</strong></span>",
+    distractores: [
+      "<i class='fa-solid fa-circle'></i><span style='padding-left:4%;'>Por el <strong>material</strong></span>", 
+      "<i class='fa-solid fa-circle'></i><span style='padding-left:4%;'>Porque esta <strong>sucia</strong></span>",
+      "<i class='fa-solid fa-circle'></i><span style='padding-left:4%;'>Por su <strong>contenido</strong></span>"
+    ],
+  },
+  {
+    pregunta: "¿Por que crees que las botellas de aceite tienen retos en su reciclaje?",
+    respuesta: "<i class='fa-solid fa-circle'></i><span style='padding-left:4%;'>por su <strong>color</strong></span>",
+    distractores: [
+      "<i class='fa-solid fa-circle'></i><span style='padding-left:4%;'>Porque quedan inpregnadas de <strong>aceite</strong></span>", 
+      "<i class='fa-solid fa-circle'></i><span style='padding-left:4%;'>Por el <strong>material</strong></span>",
+      "<i class='fa-solid fa-circle'></i><span style='padding-left:4%;'>Porque existe<strong>poco inventario </strong>en Colombia</span>"
+    ],
+  },
+  {
+    pregunta: "¿Crees que los empaques donde viene la lechuga, fresas, arándanos tienen retos en su reciclaje?",
+    respuesta: "<i class='fa-solid fa-circle'></i><span style='padding-left:4%;'><strong>si</strong></span>",
+    distractores: [
+      "<i class='fa-solid fa-circle'></i><span style='padding-left:4%;'><strong>No</strong></span>", 
+      "<i style='display:none;' class='fa-solid fa-circle'></i><span style='display:none;padding-left:4%;'></span>",
+      "<i style='display:none;' class='fa-solid fa-circle'></i><span style='display:none;padding-left:4%;'></span>"
+    ],
+  },
+  {
+    pregunta: "¿Sabes que pasa con los residuos que están quedando en los rellenos sanitarios?",
+    respuesta: "<i class='fa-solid fa-circle'></i><span style='padding-left:4%;'>Son incinerados y <strong>generan contaminación</strong></span>",
+    distractores: [
+      "<i class='fa-solid fa-circle'></i><span style='padding-left:4%;'><strong>No pasa nada,</strong> los residuos se degradan solos</span>", 
+      "<i class='fa-solid fa-circle'></i><span style='padding-left:4%;'><strong>Los tiran al agua y la </strong>contaminan</span>", 
+      "<i style='display:none;' class='fa-solid fa-circle'></i><span style='display:none;padding-left:4%;'></span>"
+    ],
+  },
+  {
+    pregunta: "¿Cuantos recicladores crees que hay en Colombia?",
+    respuesta: "<i class='fa-solid fa-circle'></i><span style='padding-left:4%;'><strong>40.000</strong></span>",
+    distractores: [
+      "<i class='fa-solid fa-circle'></i><span style='padding-left:4%;'><strong>10.000</strong></span>", 
+      "<i class='fa-solid fa-circle'></i><span style='padding-left:4%;'><strong>25.000</strong></span>", 
+      "<i style='display:none;' class='fa-solid fa-circle'></i><span style='display:none;padding-left:4%;'></span>"
+    ],
+  },
+  {
+    pregunta: "¿Sabes que camino toma el reciclaje del PET cuando sale de tu casa?",
+    respuesta: "<i class='fa-solid fa-circle'></i><span style='padding-left:4%;'>Reciclador - Bodega de reciclaje - Molino y lavado - Resinas de <strong>Andercol</strong></span>",
+    distractores: [
+      "<i class='fa-solid fa-circle'></i><span style='padding-left:4%;'>Reciclador - Molino y lavado - Resinas de <strong>Andercol</strong></span>", 
+      "<i class='fa-solid fa-circle'></i><span style='padding-left:4%;'>Reciclador - Molino y lavado - Bodega de reciclaje - Resinas de <strong>Andercol</strong></span>", 
+      "<i style='display:none;' class='fa-solid fa-circle'></i><span style='display:none;padding-left:4%;'></span>"
+    ],
+  },
+];
---
+++ index.js
@@ -0,0 +1,94 @@
+let INDEX_PREGUNTA = 0;
+let puntaje = 0;
+
+cargarPregunta(INDEX_PREGUNTA);
+
+function cargarPregunta(index) {
+  objetoPregunta = baseDePreguntas[index];
+
+  opciones = [...objetoPregunta.distractores];
+  opciones.push(objetoPregunta.respuesta);
+  for (let i = 0; i < length; i++) {
+    opciones.sort(() => Math.random() - 0.5);
+  }
+
+  document.getElementById("pregunta").innerHTML = objetoPregunta.pregunta;
+
+    document.getElementById("opcion-1").innerHTML = opciones[0];
+    document.getElementById("opcion-2").innerHTML = opciones[1];
+    document.getElementById("opcion-3").innerHTML = opciones[2];
+    document.getElementById("opcion-4").innerHTML = opciones[3];
+  
+}
+let resultadoError = function() {
+  document.querySelector('#error').style.display = 'block';;
+}
+let preguntaUno = function() {
+	document.querySelector('.uno').style.display = 'block';
+}
+let preguntaDos = function() {
+	document.querySelector('.dos').style.display = 'block';
+}
+let preguntaTres = function() {
+	document.querySelector('.tres').style.display = 'block';
+}
+async function seleccionarOpción(index) {
+
+  if (objetoPregunta === baseDePreguntas[0] ) {
+    let validezRespuesta = opciones[index] == objetoPregunta.respuesta;
+    if (validezRespuesta) {
+      await cargaUno();
+      preguntaUno();
+      puntaje++;
+    } else {
+      await resultadoError();
+    }
+}
+  else if (objetoPregunta === baseDePreguntas[1]) {
+    let validezRespuesta = opciones[index] == objetoPregunta.respuesta;
+    if (validezRespuesta) {
+      await preguntaDos();
+      cargaDos();
+      puntaje++;
+    } else {
+      await resultadoError();
+    }
+}
+else if (objetoPregunta === baseDePreguntas[2]) {
+  let validezRespuesta = opciones[index] == objetoPregunta.respuesta;
+  /* document.querySelector('#tablero-de-juego').style.backgroundImage = "url('/img/Fondo-azul.gif')"; */
+  if (validezRespuesta) {
+    await preguntaTres();
+    cargaTres();
+    
+  } else {
+    await resultadoError();;
+  }
+}
+else if (objetoPregunta === baseDePreguntas[3]) {
+  let validezRespuesta = opciones[index] == objetoPregunta.respuesta;
+  document.querySelector('#tablero-de-juego').style.backgroundImage = "url('/img/Fondo-verde.gif')";
+  if (validezRespuesta) {
+    await preguntaTres();
+    cargaTres();
+    
+  } else {
+    await resultadoError();;
+  }
+}
+  INDEX_PREGUNTA++;
+  if (INDEX_PREGUNTA >= baseDePreguntas.length) {
+    await Swal.fire({
+      title: "Juego términado",
+      text: `Tu puntaje fue de: ${puntaje}/${baseDePreguntas.length}`,
+    });
+    INDEX_PREGUNTA = 0;
+    puntaje = 0;
+  }
+  cargarPregunta(INDEX_PREGUNTA);
+
+  
+
+  
+}
+
